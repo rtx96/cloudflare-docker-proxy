fabca5f0a1099844a76a86c7151c9c02485fe536
rtx96 <50402666+rtx96@users.noreply.github.com>
2024-07-05 10:16:47 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,13 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker.199674.xyz": "https://registry-1.docker.io",
+  "quay.199674.xyz": "https://quay.io",
+  "gcr.199674.xyz": "https://gcr.io",
+  "k8s-gcr.199674.xyz": "https://k8s.gcr.io",
+  "k8s.199674.xyz": "https://registry.k8s.io",
+  "ghcr.199674.xyz": "https://ghcr.io",
+  "cloudsmith.199674.xyz": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
